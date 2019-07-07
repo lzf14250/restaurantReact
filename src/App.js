4197b0f7b00e60f83615c8b6b2e901f0ceb9c943
DESKTOP-JGS4QIP\lzf95 <zhuofu.li@wustl.edu>
2019-07-07 15:58:11 -0500

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Main from './components/MainComponent'
-import { tsConstructorType } from '@babel/types';
 
 class App extends Component {
  
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
-import Dishdetail from './DishdetailComponent';
-import { tsConstructorType } from '@babel/types';
+import DishDetail from './DishdetailComponent';
 
 class Main extends Component {
   
@@ -31,7 +30,7 @@ class Main extends Component {
         </Navbar>
         <Menu dishes={this.state.dishes} 
             onClick={(dishId) => this.onDishSelect(dishId)} />
-        <Dishdetail 
+        <DishDetail 
             dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
       </div>
     );
