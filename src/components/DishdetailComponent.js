358377048601d8b8c45a0dfc30d1da22c7e5f575
DESKTOP-JGS4QIP\lzf95 <zhuofu.li@wustl.edu>
2019-07-08 19:47:17 -0500

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({dish}) {
     if(dish == null) {
@@ -59,8 +60,7 @@ function RenderComments({comments}) {
 }
 
 function DishDetail(props) {
-    var dish = props.dish;
-    if (dish == null) {
+    if (props.dish == null) {
         return (
             <div></div>
         );
@@ -68,8 +68,20 @@ function DishDetail(props) {
         return (
             <div className="container">
                 <div className="row">
-                    <RenderDish dish={dish} />
-                    <RenderComments comments={dish.comments} />
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to={'/menu'}>Menu</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
         );
